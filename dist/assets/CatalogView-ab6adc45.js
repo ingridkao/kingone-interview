883ec56e6012dadbbe7049e91784ee621b8202e0
IngridKao <ingrid7613@gmail.com>
2023-03-01 18:35:09 +0800

    Add dist file

---
+++ dist/assets/CatalogView-ab6adc45.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,_}from"./index-2a39b7ee.js";const s={__name:"CatalogView",setup(c){return(o,t)=>(a(),e(_))}};export{s as default};
---
+++ dist/assets/VideoView-470e26a2.js
@@ -0,0 +1,1 @@
+import{o as e,c as _,_ as a}from"./index-2a39b7ee.js";const t={__name:"VideoView",setup(c){return(o,r)=>(e(),_(a))}};export{t as default};
