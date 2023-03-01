00517e24b5a9409276959c57db1c84aed88cc0de
IngridKao <ingrid7613@gmail.com>
2023-03-01 11:20:49 +0800

    Init project

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tooling</template>
+
+    This project is served and bundled with
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
+    recommended IDE setup is
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>    <br />
+    More instructions are available in <code>README.md</code>.
+  </WelcomeItem>
+</template>
